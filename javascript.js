32336aeb3f0fef8f99cc14f7aec520c5b6c2283d
Yue Sheng <a.yuesheng@gmail.com>
2024-07-03 04:01:56 +0800

    Added game animation per 5 rounds

--- javascript.js
+++ javascript.js
@@ -10,6 +10,11 @@ var wins = document.getElementById("win");
 var draws = document.getElementById("draw");
 var loss = document.getElementById("loss");
 
+var overlay = document.getElementById("overlay");
+var gameResult = document.getElementById("gameResult");
+var newRoundBtn = document.getElementById("newRoundBtn");
+
+
 // rock = 0 , paper = 1, scissors = 2
 function getRandomChoice(){
     let choice;
@@ -45,28 +50,68 @@ function checkChoice(choice,button){
 
     if(choice===(computerChoice+1)%3){
         console.log("Win");
-        result.innerHTML="Win";
+        result.innerHTML="Won Round";
         result.style.color="#1C7C54";
         wins.innerHTML = parseInt(wins.innerHTML)+1;
+        roundsPlayed++;
 
     }else if(choice === computerChoice){
-        console.log("Draw");
+        console.log("Drew Round");
         result.innerHTML="Draw";
         result.style.color="#DCF763";
         draws.innerHTML = parseInt(draws.innerHTML)+1;
+        roundsPlayed++;
     }else{
         console.log("Lose");
-        result.innerHTML="Lose";
+        result.innerHTML="Lost Round";
         result.style.color="#FF5A5F";
         loss.innerHTML = parseInt(loss.innerHTML)+1;
-       
+        roundsPlayed++;
+    }
+    if(roundsPlayed===5){
+        checkResults();
     }
 }
 
+function checkResults() {
+    let resultText,imageSrc;
+    if(parseInt(wins.innerHTML)>parseInt(loss.innerHTML)){
+        console.log("Won Game!")
+        resultText="You Won!"
+    }else if(parseInt(wins.innerHTML)<parseInt(loss.innerHTML)){
+        console.log("Lost Game")
+        resultText="You Lost"
+    }else{
+        console.log("Drew Game")
+        resultText="Draw!"
+    }
+    
+    gameResult.innerHTML = resultText
+    overlay.classList.remove("overlayOut")
+    overlay.classList.add("overlayIn")
 
+    
+}
+
+function newRound() {
+    wins.innerHTML = 0
+    draws.innerHTML = 0
+    loss.innerHTML = 0
+    roundsPlayed = 0
+    let selected = document.getElementsByClassName("selected")
+    while(selected.length !== 0){
+        selected[0].classList.remove("selected");
+    }
+    result.innerHTML="Pick a choice"
+    result.style.color="#F0EDEE"
+    overlay.classList.remove("overlayIn")
+    overlay.classList.add("overlayOut")
+}
+
+let roundsPlayed = 0
 
 
 rockButton.addEventListener("click", ()=>checkChoice(0,rockButton));
 paperButton.addEventListener("click", ()=>checkChoice(1,paperButton));
 scissorButton.addEventListener("click", ()=>checkChoice(2,scissorButton));
-
+newRoundBtn.addEventListener("click",()=>newRound())
